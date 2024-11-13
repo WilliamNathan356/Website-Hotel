67fea2fdc76d783995d607818137a3873a8a4106
Gareth Glendiuz Yauwira <92643002+OMCSB@users.noreply.github.com>
2024-11-13 22:44:21 +0800

    Implement proper awati/async functionality

--- controller/authorizationController.js
+++ controller/authorizationController.js
@@ -17,13 +17,12 @@ const generateAccessToken = (email, id) => {
 };
 
 module.exports = {
-    register: (req,res) => {
-        const payload = req.body;
-        
-        userModel.createUser(
-            Object.assign(payload)
-        )
-        .then((user) => {
+    async register(req, res) {
+        try {
+            const payload = req.body;
+            const user = await userModel.createUser(
+                Object.assign(payload)
+            );
             const accessToken = generateAccessToken(payload.email, user.id);
 
             return res.status(200).json({
@@ -33,38 +32,28 @@ module.exports = {
                     token: accessToken,
                 },
             });
-        })
-        .catch((err) => {
+        } catch (err) {
             return res.status(500).json({
                 status: false,
                 error: err,
             });
-        });
+        }
     },
-    login: (req, res) => {
-        const { email, password } = req.body;
+    async login(req, res) {
+        try {
+            const { email, password } = req.body;
+            const user = await userModel.findUser({ email });
 
-        userModel.findUser({ email })
-        .then((user) => {
-            if (!user){
+            if (!user || !user.password) {
                 return res.status(400).json({
                     status: false,
                     error: {
                         message: "Email/Password is incorrect!"
                     }
-                })
+                });
             }
 
-            if (!user.password){
-                return res.status(400).json({
-                    status: false,
-                    error: {
-                        message: "Email/Password is incorrect!"
-                    }
-                })
-            }
-
-            const accessToken = generateAccessToken(user.email, user.id)
+            const accessToken = generateAccessToken(user.email, user.id);
 
             return res.status(200).json({
                 status: true,
@@ -72,13 +61,13 @@ module.exports = {
                     user: user.toJSON(),
                     token: accessToken,
                 }
-            })
-        })
-        .catch((err) =>{
+            });
+        } catch (err) {
             return res.status(500).json({
                 status: false,
                 error: err,
-            })
-        });
+            });
+        }
     }
+
 }
--- controller/userController.js
+++ controller/userController.js
@@ -1,12 +1,12 @@
 const userModel = require("../model/user");
 
 module.exports = {
-    getUser: (req, res) => {
+    async getUser(req, res) {
         const {
             user: { userId },
         } = req;
 
-        userModel.findUser({ id: userId })
+        await userModel.findUser({ id: userId })
         .then((user) => {
             return res.status(200).json({
                 status: true,
