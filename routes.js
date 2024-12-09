2f99f52097e36b8683311fbdcf068976eb774b25
Gareth Glendiuz Yauwira <92643002+OMCSB@users.noreply.github.com>
2024-12-10 03:31:42 +0800

    Add Additional Routes for Booking and Finding Room

--- routes.js
+++ routes.js
@@ -5,13 +5,18 @@ const router = require("express").Router();
 const userController = require("./controller/userController");
 const roomController = require("./controller/roomController")
 const authorizationController = require("./controller/authorizationController");
+const bookingController = require("./controller/bookingController");
 
 // Register/Login
 router.post("/register", authorizationController.register);
 router.post("/login", authorizationController.login);
 
 // Rooms
-router.post("/findRooms", roomController.getRooms)
+router.post("/findRooms", roomController.getRooms);
+router.post("/findRoom", roomController.findRoom);
+
+// Room Booking
+router.post('/book', bookingController.bookRoom);
 
 // User
 router.get("/:userId", userController.getUser)
