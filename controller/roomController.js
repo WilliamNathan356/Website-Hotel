07468fdcce134001917a3ca24e5b9247b5fee940
Gareth Glendiuz Yauwira <92643002+OMCSB@users.noreply.github.com>
2024-12-10 03:30:22 +0800

    Add findRoom Function

--- controller/roomController.js
+++ controller/roomController.js
@@ -2,10 +2,39 @@ const roomModel = require("../model/room");
 const moment = require("moment");
 
 module.exports = {
+    async findRoom(req, res){
+        try {
+            const payload = req.body;
+            const uName = payload.uName
+            
+            const room = JSON.parse(JSON.stringify(await roomModel.findRoom(uName)));
+
+            if (Object.keys(room).length > 0){
+                const response = {
+                    roomName: room.roomName
+                }
+                res.send(response);
+            } else {
+                const response = {
+                    status: "No Rooms Available",
+                }
+                res.send(response);
+            }
+        } catch(err) {
+            console.log(err);
+            const response = {
+                status: 500,
+                error: err
+            };
+            res.send(response);
+        }
+    },
+
     async getRooms(req, res) {
         try {
             // Set Payload
             const payload = req.body;
+            console.log(payload);
             const date = moment(payload.date).format('YYYY-MM-DD');
             const guest = payload.guestNum;
             const location = payload.location;
